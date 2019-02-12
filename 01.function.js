e2d78c9483efacf5aa1d1f10e310b7ee3633548c
amicius <admin@amicius.fr>
2019-02-12 12:47:39 +0100

    functions

--- 01.function.js
+++ 01.function.js
@@ -2,20 +2,30 @@ console.log("01 - Fonctions");
 
 var nb1 = 10, nb2 = 20;
 
-function additionner(nb1,nb2){
+function additionner(nb1, nb2) {
     return nb1 + nb2;
 }
 
 var resultat1 = additionner(nb1, nb2);
-console.log('Resultat 1 = ',resultat1);
+console.log('Resultat 1 = ', resultat1);
 
 var somme = additionner;
 
-var resultat2 = somme(nb1,nb2);
-console.log('Resultat 2 = ',resultat2);
+var resultat2 = somme(nb1, nb2);
+console.log('Resultat 2 = ', resultat2);
 
-var multiplication = function (nb1, nb2){
+var multiplication = function (nb1, nb2) {
     return nb1 * nb2;
 }
 var resultat3 = multiplication(nb1, nb2);
-console.log('Resultat 3 = ',resultat3);
+console.log('Resultat 3 = ', resultat3);
+
+var afficherOperation = function (monOperation, operation, nb1, nb2) {
+    console.log(monOperation, '(', nb1, ',', nb2, ') = ', operation(nb1, nb2));
+}
+
+afficherOperation("Somme", somme, 20, 40);
+afficherOperation("Multiplication", multiplication, 10, 20);
+afficherOperation("Soustraction", function (nb1, nb2) {
+    return nb1 - nb2;
+}, 15, 5);

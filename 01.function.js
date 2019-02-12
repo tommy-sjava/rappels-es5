018ba8caf14dc7339b2fed6dec2a7d08be901993
Desplat <matt-desplat@wanadoo.fr>
2019-02-12 12:20:38 +0100

    Fichier 01.function complet

--- 01.function.js
+++ 01.function.js
@@ -1,1 +1,32 @@
-console.log("01 - Fonctions");
+lg = console.log;
+lg("01 - Fonctions");
+
+var nombre1 = 10;
+var nombre2 = 20;
+
+function additionner(nb1, nb2) {
+    return nb1 + nb2;
+}
+
+var resultat1 = additionner(nombre1, nombre2);
+lg('resultat1 =', resultat1);
+
+var somme = additionner;
+
+var resultat2 = somme(nombre1, nombre2);
+lg('resultat2 =', resultat2);
+
+var multiplication = function(nb1, nb2){
+    return nb1 * nb2;
+}
+
+var resultat3 = multiplication(nombre1, nombre2);
+lg('resultat3 =', resultat3);
+
+var afficherOperation = function(nomOperation, operation, nb1, nb2) {
+    lg(nomOperation, '(',nb1, ',', nb2, ') = ', operation(nb1, nb2));
+}
+
+afficherOperation('Somme', somme, 20, 40);
+afficherOperation('Multiplication', multiplication, 10, 20);
+afficherOperation('Soustraction', function(nb1, nb2){return nb1 - nb2}, 15, 5);

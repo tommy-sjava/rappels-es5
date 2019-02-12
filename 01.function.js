0558499423c1daf9dca6fd592692c2f38708a98b
amicius <admin@amicius.fr>
2019-02-12 12:01:47 +0100

    peron corentin

--- 01.function.js
+++ 01.function.js
@@ -1,1 +1,21 @@
 console.log("01 - Fonctions");
+
+var nb1 = 10, nb2 = 20;
+
+function additionner(nb1,nb2){
+    return nb1 + nb2;
+}
+
+var resultat1 = additionner(nb1, nb2);
+console.log('Resultat 1 = ',resultat1);
+
+var somme = additionner;
+
+var resultat2 = somme(nb1,nb2);
+console.log('Resultat 2 = ',resultat2);
+
+var multiplication = function (nb1, nb2){
+    return nb1 * nb2;
+}
+var resultat3 = multiplication(nb1, nb2);
+console.log('Resultat 3 = ',resultat3);
